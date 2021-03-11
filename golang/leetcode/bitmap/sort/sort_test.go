7ddc322e8daa15ad85d852bb35ea683f2fa76a7a
xie4ever <xie4ever@qq.com>
2021-03-12 01:31:49 +0800

    feat(project): perf my practice

---
+++ golang/leetcode/bitmap/sort/sort_test.go
@@ -0,0 +1,65 @@
+package sort
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+type bitMap struct {
+	bits []byte
+}
+
+// 初始化一个BitMap
+// 一个byte有8位，可代表8个数字，除8后加1为存放最大数所需的容量
+func newBitMap(max int) *bitMap {
+	bits := make([]byte, (max/8)+1)
+	return &bitMap{bits: bits}
+}
+
+// 1.找到所属的int，num / 8
+// 2.找到在这个int中1的位移，num % 8
+// 3.设置1，b[byteIndex] = b[byteIndex] | (1 << bitIndex)
+func (b *bitMap) add(num int) error {
+	byteIndex := num / 8
+	if byteIndex >= len(b.bits) {
+		return errors.New("num > max")
+	}
+	bitIndex := num % 8
+	b.bits[byteIndex] = b.bits[byteIndex] | (1 << bitIndex)
+	return nil
+}
+
+// 1.找到所属的int，num / 8
+// 2.找到在这个int中1的位移，num % 8
+// 3.设置0，b[byteIndex] = b[byteIndex] & (^(1 << bitIndex))
+func (b *bitMap) clear(num int) error {
+	byteIndex := num / 8
+	if byteIndex >= len(b.bits) {
+		return errors.New("num > max")
+	}
+	bitIndex := num % 8
+	b.bits[byteIndex] = b.bits[byteIndex] & (^(1 << bitIndex))
+	return nil
+}
+
+// 1.找到所属的int，num / 8
+// 2.找到在这个int中1的位移，num % 8
+// 3. b[byteIndex] & (1 << bitIndex)
+func (b *bitMap) contain(num int) bool {
+	byteIndex := num / 8
+	if byteIndex >= len(b.bits) {
+		return false
+	}
+	bitIndex := num % 8
+	return !(b.bits[byteIndex]&(1<<bitIndex) == 0)
+}
+
+// TestBitMap ...
+func TestBitMap(t *testing.T) {
+	b := newBitMap(100)
+	b.add(3)
+	fmt.Println(b.contain(3))
+	b.clear(3)
+	fmt.Println(b.contain(3))
+}
