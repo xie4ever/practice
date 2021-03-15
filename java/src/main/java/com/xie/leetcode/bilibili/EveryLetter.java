8a8a034d2b607dcef9154e098ffcac7afc517de5
xie4ever <xie4ever@qq.com>
2021-03-15 16:50:31 +0800

    feat(feat): add java practice

---
+++ java/src/main/java/com/xie/leetcode/bilibili/EveryLetter.java
@@ -0,0 +1,11 @@
+package com.xie.leetcode.bilibili;
+
+/**
+ * @author xie4ever
+ * @date 2021/3/15 16:42
+ */
+public class EveryLetter {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
