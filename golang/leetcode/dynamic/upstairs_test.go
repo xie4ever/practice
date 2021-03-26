a6792c546a24eaf777af43ed5faf37e952973465
xie4ever <xie4ever@qq.com>
2021-03-26 22:04:19 +0800

    feat(sort): perf practice

--- golang/leetcode/dynamic/upstairs_test.go
+++ golang/leetcode/dynamic/upstairs_test.go
@@ -44,3 +44,28 @@ func upstairs2(step int) int {
 func TestUpStairs2(t *testing.T) {
 	fmt.Println(upstairs2(10))
 }
+
+func upstairs3(step int) int {
+	if step == 1 {
+		return 1
+	}
+	if step == 2 {
+		return 2
+	}
+	var (
+		a   = 1
+		b   = 2
+		tmp = 0
+	)
+	for i := 3; i < step; i++ {
+		tmp = a + b
+		a = b
+		b = tmp
+	}
+	return tmp
+}
+
+// TestUpStairs3 ...
+func TestUpStairs3(t *testing.T) {
+	fmt.Println(upstairs3(10))
+}
