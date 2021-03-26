dc6ae2508160032a5635b95015d45b2e6653098e
xie4ever <xie4ever@qq.com>
2021-03-26 21:11:34 +0800

    feat(sort): add practice

---
+++ golang/leetcode/dynamic/upstairs_test.go
@@ -0,0 +1,46 @@
+package dynamic
+
+import (
+	"fmt"
+	"testing"
+)
+
+// TestUpStair ...
+func TestUpStairs(t *testing.T) {
+	fmt.Println(upstairs(10))
+}
+
+func upstairs(step int) int {
+	if step == 1 {
+		return 1
+	}
+	if step == 2 {
+		return 2
+	}
+	return upstairs(step-1) + upstairs(step-2)
+}
+
+func upstairs2(step int) int {
+	if step == 1 {
+		return 1
+	}
+	if step == 2 {
+		return 2
+	}
+	var (
+		a   = 1
+		b   = 2
+		tmp = 0
+	)
+	for i := 3; i <= step; i++ {
+		tmp = a + b
+		a = b
+		b = tmp
+	}
+	return tmp
+}
+
+// TestUpStairs2 ...
+func TestUpStairs2(t *testing.T) {
+	fmt.Println(upstairs2(10))
+}
