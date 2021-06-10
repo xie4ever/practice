69fffbd2fad2fb201022da6d55f64a32030b3df8
xie4ever <xie4ever@qq.com>
2021-06-11 00:03:25 +0800

    feat(golang): add some test

--- golang/concurrent/test/task_test.go
+++ golang/concurrent/test/task_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"testing"
@@ -31,4 +32,68 @@ func TestCal(t *testing.T) {
 	}
 	fmt.Println(answer)
 	fmt.Println(time.Now().Second() - start)
+}
+
+func cal2(ch chan int, second int) {
+	time.Sleep(time.Duration(second) * time.Second)
+	ch <- 1
+}
+
+// TestCal2 ...
+func TestCal2(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		time.Sleep(time.Duration(3) * time.Second)
+		cancel()
+	}()
+
+	ch := make(chan int, 1)
+	go cal2(ch, 10)
+
+	select {
+	case i, _ := <-ch:
+		fmt.Println(i)
+	case <-ctx.Done():
+		fmt.Println("ctx break")
+	}
+}
+
+func cal3(ctx context.Context, second int) {
+	time.Sleep(time.Duration(second) * time.Second) // 这里一直阻塞了
+	select {
+	case <-ctx.Done():
+		fmt.Println("ctx break")
+	default:
+		fmt.Println(second)
+	}
+}
+
+// TestCal3 ...
+func TestCal3(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	for i := 0; i < 10; i++ {
+		go cal3(ctx, i)
+	}
+
+	time.Sleep(time.Duration(5) * time.Second)
+	cancel()
+
+	time.Sleep(time.Duration(5) * time.Second)
+}
+
+// TestCal4 ...
+func TestCal4(t *testing.T) {
+	go func() {
+		go func() {
+			time.Sleep(time.Duration(5) * time.Second)
+			fmt.Println("son quit")
+		}()
+
+		time.Sleep(time.Duration(3) * time.Second)
+		fmt.Println("father quit")
+	}()
+
+	time.Sleep(time.Duration(10) * time.Second)
 }
